0f273f242967d755f547daf0e22560238166ebf1
guseyn <guseynrim@gmail.com>
2022-04-14 09:36:01 +0300

    public acess to methods

--- Sources/CoreTableView/Extensions/Ext+String.swift
+++ Sources/CoreTableView/Extensions/Ext+String.swift
@@ -9,13 +9,13 @@ import UIKit
 
 extension String {
     
-    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+    public func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
         let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
         return ceil(boundingBox.height)
     }
     
-    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+    public func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
         let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
         let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
         return ceil(boundingBox.width)
@@ -24,7 +24,7 @@ extension String {
 
 extension NSAttributedString {
 
-    func height(containerWidth: CGFloat) -> CGFloat {
+    public func height(containerWidth: CGFloat) -> CGFloat {
 
         let rect = self.boundingRect(
             with: CGSize.init(width: containerWidth, height: CGFloat.greatestFiniteMagnitude),
@@ -34,7 +34,7 @@ extension NSAttributedString {
         return ceil(rect.size.height)
     }
 
-    func width(containerHeight: CGFloat) -> CGFloat {
+    public func width(containerHeight: CGFloat) -> CGFloat {
         let rect = self.boundingRect(
             with: CGSize.init(width: CGFloat.greatestFiniteMagnitude, height: containerHeight),
             options: [.usesLineFragmentOrigin, .usesFontLeading],
